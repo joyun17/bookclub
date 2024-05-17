e1748df1037e5a22d8cec9dc91ac8dfa8f7179f3
joyun <oto11914@naver.com>
2024-05-17 17:43:01 +0900

    feat : 좋아요 기능 추가

---
+++ src/main/java/org/fullstack4/bookclub/mapper/LikeMapper.java
@@ -0,0 +1,22 @@
+package org.fullstack4.bookclub.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.fullstack4.bookclub.domain.MemberVO;
+import org.fullstack4.bookclub.domain.ShareVO;
+import org.fullstack4.bookclub.domain.StudyVO;
+import org.fullstack4.bookclub.dto.PageRequestDTO;
+
+import java.util.List;
+
+public interface ShareMapper {
+    int regist(@Param("member_id")String member_id, @Param("study_idx")int study_idx);
+    List<ShareVO> shareList(int study_idx);
+
+    int shareStudyTotalCount(PageRequestDTO requestDTO);
+    List<StudyVO> shareStudyListByPage(PageRequestDTO requestDTO);
+    int shareStudyTotalCount2(PageRequestDTO requestDTO);
+    List<StudyVO> shareStudyListByPage2(PageRequestDTO requestDTO);
+    List<ShareVO> listAll(String member_id);
+    int delete(int study_idx);
+
+}
---
+++ src/main/java/org/fullstack4/bookclub/service/LikeServiceImpl.java
@@ -0,0 +1,97 @@
+package org.fullstack4.bookclub.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.fullstack4.bookclub.domain.MemberVO;
+import org.fullstack4.bookclub.domain.StudyVO;
+import org.fullstack4.bookclub.dto.*;
+import org.fullstack4.bookclub.mapper.MemberMapper;
+import org.fullstack4.bookclub.mapper.ShareMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class ShareServiceImpl implements ShareServiceIf{
+
+    private final MemberMapper memberMapper;
+    private final ShareMapper shareMapper;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public int regist(String member_id, int study_idx) {
+        int result = shareMapper.regist(member_id, study_idx);
+        return result;
+    }
+
+    @Override
+    public List<ShareDTO> shareList(int study_idx) {
+        List<ShareDTO> shareDTOList = shareMapper.shareList(study_idx).stream()
+                .map(shareVO -> modelMapper.map(shareVO, ShareDTO.class))
+                .collect(Collectors.toList());
+        return shareDTOList;
+    }
+
+    @Override
+    public int ShareStudyTotalCount(PageRequestDTO requestDTO) {
+        return shareMapper.shareStudyTotalCount(requestDTO);
+    }
+
+    @Override
+    public PageResponseDTO<StudyDTO> ShareStudyListByPage(PageRequestDTO pageRequestDTO) {
+        List<StudyVO> voList = shareMapper.shareStudyListByPage(pageRequestDTO);
+        log.info(voList);
+        List<StudyDTO> dtoList = voList.stream()
+                .map(vo->modelMapper.map(vo,StudyDTO.class))
+                .collect(Collectors.toList());
+        int total_count = shareMapper.shareStudyTotalCount(pageRequestDTO);
+
+        PageResponseDTO<StudyDTO> responseDTO = PageResponseDTO.<StudyDTO>withAll()
+                .requestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total_count(total_count)
+                .build();
+
+        return responseDTO;
+    }
+
+    @Override
+    public int ShareStudyTotalCount2(PageRequestDTO requestDTO) {
+        return shareMapper.shareStudyTotalCount2(requestDTO);
+    }
+
+    @Override
+    public PageResponseDTO<StudyDTO> ShareStudyListByPage2(PageRequestDTO pageRequestDTO) {
+        List<StudyVO> voList = shareMapper.shareStudyListByPage2(pageRequestDTO);
+        List<StudyDTO> dtoList = voList.stream()
+                .map(vo->modelMapper.map(vo,StudyDTO.class))
+                .collect(Collectors.toList());
+        int total_count = shareMapper.shareStudyTotalCount2(pageRequestDTO);
+
+        PageResponseDTO<StudyDTO> responseDTO = PageResponseDTO.<StudyDTO>withAll()
+                .requestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total_count(total_count)
+                .build();
+
+        return responseDTO;
+    }
+
+    @Override
+    public List<ShareDTO> listAll(String member_id) {
+        List<ShareDTO> shareDTOList = shareMapper.listAll(member_id).stream()
+                .map(shareVO -> modelMapper.map(shareVO, ShareDTO.class))
+                .collect(Collectors.toList());
+        return shareDTOList;
+    }
+
+    @Override
+    public int delete(int study_idx) {
+        int result = shareMapper.delete(study_idx);
+        return result;
+    }
+}
